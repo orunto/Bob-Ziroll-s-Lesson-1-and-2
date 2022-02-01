6fdc9390126daeb6fcceeaf50dd8bbd53fb23bf9
orunto <98480650+orunto@users.noreply.github.com>
2022-02-01 16:55:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 ReactDOM.render(<h1>My first react code</h1>, document.getElementById("root"))
+ReactDOM.render(<ul><li>My first react list item</li><li>My second react list item</li></ul>, document.getElementById("root2"))
